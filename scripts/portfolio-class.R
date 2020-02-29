94c1c3129c6fa0efa504f7f2a7cb2221a213e0bb
r-wickham <46103664+r-wickham@users.noreply.github.com>
2020-02-29 10:49:22 -0800

    setup webapp and class scripts

---
+++ scripts/portfolio-class.R
@@ -0,0 +1,25 @@
+#Class definition for the main object that stores
+#  income, expenses, and methods for computing
+
+
+
+portfolio <- list()
+portfolio$income <- list()
+portfolio$expenses <- list()
+
+
+### Attributes ------------------------------------------------------------------------------------
+
+
+
+
+### Methods ---------------------------------------------------------------------------------------
+
+
+createNewIncome <- function( name, years  ){
+  #
+  #Adds a new data.frame to the income list
+  
+  portfolio
+  
+}
